03358e9036f89f04f8cec87c64add264059854e8
Yaroslav Yakubenko <yyakuben@c2r5s7.42wolfsburg.de>
2024-09-28 23:18:09 +0200

    remake big functions to small

--- srcs/parse/parse_input.c
+++ srcs/parse/parse_input.c
@@ -6,7 +6,7 @@
 /*   By: yyakuben <yyakuben@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/20 17:40:27 by yyakuben          #+#    #+#             */
-/*   Updated: 2024/09/27 16:27:15 by yyakuben         ###   ########.fr       */
+/*   Updated: 2024/09/28 22:15:49 by yyakuben         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,228 +45,447 @@ void	ft_print_doulbe_pointer(char **str)
 	}
 }
 
+int	handle_quote(char *cmd, int i, char quote_char)
+{
+    i++;
+    while (cmd[i] && cmd[i] != quote_char)
+        i++;
+    return (i + 1);
+}
 int	ft_get_parts(char *cmd)
 {
-	int	i;
-	int	k;
+    int	i;
+    int	k;
 
 	i = 0;
 	k = 0;
-	while (cmd[i])
-	{
-		if (cmd[i] == '\'')
-		{
-			i++;
-			k++;
-			while (cmd[i] && cmd[i] != '\'')
-				i++;
-			i++;
-		}
-		else if (cmd[i] == '\"')
-		{
-			i++;
-			k++;
-			while (cmd[i] && cmd[i] != '\"')
-				i++;
-			i++;
-		}
-		else if(cmd[i] == ' ')
-			i++;
-		else if(cmd[i] != ' ')
-		{
-			k++;
-			while (cmd[i] && cmd[i] != ' ')
-				i++;
-		}
-	}
-	return (k);
+    while (cmd[i])
+    {
+        if (cmd[i] == '\'' || cmd[i] == '\"')
+        {
+            i = handle_quote(cmd, i, cmd[i]);
+            k++;
+        }
+        else if (cmd[i] == ' ')
+            i++;
+        else
+        {
+            k++;
+            while (cmd[i] && cmd[i] != ' ' && cmd[i] != '\'' && cmd[i] != '\"')
+                i++;
+        }
+    }
+    return (k);
 }
 
-char	**ft_deal_cmd(char *cmd)
+
+
+// int	ft_get_parts(char *cmd)
+// {
+// 	int	i;
+// 	int	k;
+
+// 	i = 0;
+// 	k = 0;
+// 	while (cmd[i])
+// 	{
+// 		if (cmd[i] == '\'')
+// 		{
+// 			i++;
+// 			k++;
+// 			while (cmd[i] && cmd[i] != '\'')
+// 				i++;
+// 			i++;
+// 		}
+// 		else if (cmd[i] == '\"')
+// 		{
+// 			i++;
+// 			k++;
+// 			while (cmd[i] && cmd[i] != '\"')
+// 				i++;
+// 			i++;
+// 		}
+// 		else if(cmd[i] == ' ')
+// 			i++;
+// 		else if(cmd[i] != ' ')
+// 		{
+// 			k++;
+// 			while (cmd[i] && cmd[i] != ' ')
+// 				i++;
+// 		}
+// 	}
+// 	return (k);
+// }
+
+static char *create_substring(const char *cmd, int start, int end)
+{
+    char *substr;
+	
+	substr = malloc(sizeof(char) * (end - start + 1));
+    if (!substr)
+        return (NULL);
+    strncpy(substr, &cmd[start], end - start);
+    substr[end - start] = '\0';
+    return (substr);
+}
+
+static char *extract_string(char *cmd, int *i, char quote_char)
+{
+	char	*str;
+    int		l;
+    int		j;
+
+	j = 0;
+	l = *i;
+    (*i)++;
+    while (cmd[*i] && cmd[*i] != quote_char)
+    {
+        (*i)++;
+        j++;
+    }
+    str = malloc(sizeof(char) * (j + 1));
+    if (!str)
+        return (NULL);
+    strncpy(str, &cmd[l], j);
+    str[j] = '\0';
+    (*i)++;
+    return (str);
+}
+
+static void fill_args(char **args, char *cmd)
+{
+	char	*str;
+    int 	i;
+    int 	k;
+
+	i = 0;
+	k = 0;
+    while (cmd[i])
+    {
+        if (cmd[i] == '\'' || cmd[i] == '\"')
+        {
+            str = extract_string(cmd, &i, cmd[i]);
+            if (!str)
+                return;
+            args[k++] = str;
+        }
+        else if (cmd[i] != ' ')
+        {
+            int l = i;
+            while (cmd[i] && cmd[i] != ' ')
+                i++;
+            args[k++] = create_substring(cmd, l, i); // Создание подстроки
+        }
+        else
+            i++;
+    }
+}
+
+char **ft_deal_cmd(char *cmd)
 {
 	char	**args;
-	int		i;
-	int		j;
-	int		k;
-	int		l;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	i = 0;
-	while (cmd[i])
-	{
-		if (cmd[i] == '\'' && cmd[i] == '\"')
-			j = 1;
-		i++;
-	}
-	if (j == 1)
-		args = ft_split(cmd, ' ');
-	else
-	{
-		i = 0;
-		k = ft_get_parts(cmd);
-		args = malloc(sizeof(char *) * (k + 1));
-		if (!args)
-			return (NULL);
-		while (k >= 0)
-		{
-			args[k] = NULL;
-			k--;
-		}
-		i = 0;
-		k = 0;
-		while (cmd[i])
-		{
-			if (cmd[i] == '\'')
-			{
-				i++;
-				l = i;
-				j = 0;
-				while (cmd[i] != 39)
-				{
-					i++;
-					j++;
-				}
-				args[k] = malloc(sizeof(char) * (j + 1));
-				if (!args[k])
-					return (NULL);
-				j = 0;
-			
-				while (l < i)
-				{
-					args[k][j] = cmd[l];
-					l++;
-					j++;
-				}
-				args[k][j] = '\0';
-				k++;
-				i++;
-			}
-			else if (cmd[i] == '\"')
-			{
-				i++;
-				l = i;
-				j = 0;
-				while (cmd[i] && cmd[i] != 34)
-				{
-					i++;
-					j++;
-				}
-				args[k] = malloc(sizeof(char) * (j + 1));
-				if (!args[k])
-					return (NULL);
-				j = 0;
-				while (l < i)
-				{
-					args[k][j] = cmd[l];
-					l++;
-					j++;
-				}
-				args[k][j] = '\0';
-				k++;
-				i++;
-			}
-			else if (cmd[i] == ' ')
-				i++;
-			else if (cmd[i] != ' ')
-			{
-				l = i;
-				j = 0;
-				while (cmd[i] && cmd[i] != ' ')
-				{
-					i++;
-					j++;
-				}
-				args[k] = malloc(sizeof(char) * (j + 1));
-				if (!args[k])
-					return (NULL);
-				j = 0;
-				while (l < i)
-				{
-					args[k][j] = cmd[l];
-					l++;
-					j++;
-				}
-				args[k][j] = '\0';
-				k++;
-			}
-		}
-	}
-	return (args);
+    int		parts_count;
+	
+	parts_count = ft_get_parts(cmd);
+    args = malloc(sizeof(char *) * (parts_count + 1));
+    if (!args)
+        return (NULL);
+    args[parts_count] = NULL;
+    fill_args(args, cmd);
+    return (args);
 }
 
+
+// char	**ft_deal_cmd(char *cmd)
+// {
+// 	char	**args;
+// 	int		i;
+// 	int		j;
+// 	int		k;
+// 	int		l;
+
+// 	i = 0;
+// 	j = 0;
+// 	k = 0;
+// 	i = 0;
+// 	while (cmd[i])
+// 	{
+// 		if (cmd[i] == '\'' && cmd[i] == '\"')
+// 			j = 1;
+// 		i++;
+// 	}
+// 	if (j == 1)
+// 		args = ft_split(cmd, ' ');
+// 	else
+// 	{
+// 		i = 0;
+// 		k = ft_get_parts(cmd);
+// 		args = malloc(sizeof(char *) * (k + 1));
+// 		if (!args)
+// 			return (NULL);
+// 		while (k >= 0)
+// 		{
+// 			args[k] = NULL;
+// 			k--;
+// 		}
+// 		i = 0;
+// 		k = 0;
+// 		while (cmd[i])
+// 		{
+// 			if (cmd[i] == '\'')
+// 			{
+// 				i++;
+// 				l = i;
+// 				j = 0;
+// 				while (cmd[i] != 39)
+// 				{
+// 					i++;
+// 					j++;
+// 				}
+// 				args[k] = malloc(sizeof(char) * (j + 1));
+// 				if (!args[k])
+// 					return (NULL);
+// 				j = 0;
+			
+// 				while (l < i)
+// 				{
+// 					args[k][j] = cmd[l];
+// 					l++;
+// 					j++;
+// 				}
+// 				args[k][j] = '\0';
+// 				k++;
+// 				i++;
+// 			}
+// 			else if (cmd[i] == '\"')
+// 			{
+// 				i++;
+// 				l = i;
+// 				j = 0;
+// 				while (cmd[i] && cmd[i] != 34)
+// 				{
+// 					i++;
+// 					j++;
+// 				}
+// 				args[k] = malloc(sizeof(char) * (j + 1));
+// 				if (!args[k])
+// 					return (NULL);
+// 				j = 0;
+// 				while (l < i)
+// 				{
+// 					args[k][j] = cmd[l];
+// 					l++;
+// 					j++;
+// 				}
+// 				args[k][j] = '\0';
+// 				k++;
+// 				i++;
+// 			}
+// 			else if (cmd[i] == ' ')
+// 				i++;
+// 			else if (cmd[i] != ' ')
+// 			{
+// 				l = i;
+// 				j = 0;
+// 				while (cmd[i] && cmd[i] != ' ')
+// 				{
+// 					i++;
+// 					j++;
+// 				}
+// 				args[k] = malloc(sizeof(char) * (j + 1));
+// 				if (!args[k])
+// 					return (NULL);
+// 				j = 0;
+// 				while (l < i)
+// 				{
+// 					args[k][j] = cmd[l];
+// 					l++;
+// 					j++;
+// 				}
+// 				args[k][j] = '\0';
+// 				k++;
+// 			}
+// 		}
+// 	}
+// 	return (args);
+// }
+
+int	handle_double_redirect(char *cmd_line, int i)
+{
+    i += 3;
+    while (cmd_line[i] && cmd_line[i] != ' ')
+        i++;
+    i++;
+    return (i);
+}
+int	handle_single_redirect(char *cmd_line, int i)
+{
+    i += 2;
+    while (cmd_line[i] && cmd_line[i] != ' ')
+        i++;
+    i++;
+    return (i);
+}
 int	ft_find_end(char *cmd_line, int i)
 {
-	if (cmd_line[i] == '>' && cmd_line[i + 1] == '>')
-	{
-		i += 3;
-		while (cmd_line[i] && cmd_line[i] != ' ')
-			i++;
-		i++;
-	}
-	else if (cmd_line[i] == '<' && cmd_line[i + 1] == '<')
-	{
-		i += 3;
-		while (cmd_line[i] && cmd_line[i] != ' ')
-			i++;
-		i++;
-	}
-	else if (cmd_line[i] == '>' || cmd_line[i] == '<')
-	{
-		i += 2;
-		while (cmd_line[i] && cmd_line[i] != ' ')
-			i++;
-		i++;
-	}
-	else if (cmd_line[i] == '|')
-		i += 2;
-	else
-	{
-		while (cmd_line[i] && cmd_line[i] != '|' && cmd_line[i] != '<' && cmd_line[i] != '>')
-			i++;
-	}
-	return (i);
+    if ((cmd_line[i] == '>' && cmd_line[i + 1] == '>') || 
+        (cmd_line[i] == '<' && cmd_line[i + 1] == '<'))
+        return (handle_double_redirect(cmd_line, i));
+    else if (cmd_line[i] == '>' || cmd_line[i] == '<')
+        return (handle_single_redirect(cmd_line, i));
+    else if (cmd_line[i] == '|')
+        return (i + 2);
+    else
+    {
+        while (cmd_line[i] && cmd_line[i] != '|' && cmd_line[i] != '<' && cmd_line[i] != '>')
+            i++;
+    }
+    return (i);
 }
 
-t_cmd	*ft_start_parse(char *cmd_line, char **evnp)
-{
-	int		i;
-	int		j;
-	char 	*cmd;
-	t_cmd	*cmd_list;
-	t_cmd	*new_cmd;
-	t_cmd	*tmp;
 
+// int	ft_find_end(char *cmd_line, int i)
+// {
+// 	if (cmd_line[i] == '>' && cmd_line[i + 1] == '>')
+// 	{
+// 		i += 3;
+// 		while (cmd_line[i] && cmd_line[i] != ' ')
+// 			i++;
+// 		i++;
+// 	}
+// 	else if (cmd_line[i] == '<' && cmd_line[i + 1] == '<')
+// 	{
+// 		i += 3;
+// 		while (cmd_line[i] && cmd_line[i] != ' ')
+// 			i++;
+// 		i++;
+// 	}
+// 	else if (cmd_line[i] == '>' || cmd_line[i] == '<')
+// 	{
+// 		i += 2;
+// 		while (cmd_line[i] && cmd_line[i] != ' ')
+// 			i++;
+// 		i++;
+// 	}
+// 	else if (cmd_line[i] == '|')
+// 		i += 2;
+// 	else
+// 	{
+// 		while (cmd_line[i] && cmd_line[i] != '|' && cmd_line[i] != '<' && cmd_line[i] != '>')
+// 			i++;
+// 	}
+// 	return (i);
+// }
+
+char *create_command_substring(const char *cmd_line, int j, int i)
+{
+    char *cmd;
+	
+	cmd = malloc(sizeof(char) * (i - j + 1));
+    if (!cmd)
+        return (NULL);
+    ft_strlcpy(cmd, &cmd_line[j], i - j + 1);
+    return (cmd);
+}
+t_cmd *create_and_fill_cmd(char *cmd, char **evnp)
+{
+    t_cmd *new_cmd;
+	
+	new_cmd = init_cmd(evnp);
+    if (!new_cmd)
+        return (NULL);
+    free(new_cmd->args);
+    new_cmd->args = ft_deal_cmd(cmd);
+    return (new_cmd);
+}
+static void add_command_to_list(t_cmd **cmd_list, t_cmd *new_cmd)
+{
+    t_cmd *tmp;
+
+	tmp = *cmd_list;
+    if (!tmp)
+        *cmd_list = new_cmd;
+    else
+    {
+        while (tmp->next)
+            tmp = tmp->next;
+        tmp->next = new_cmd;
+    }
+}
+
+static char *extract_command(char *cmd_line, int *i)
+{
+    int j;
+	
+	j = *i;
+    *i = ft_find_end(cmd_line, *i);
+    return (create_command_substring(cmd_line, j, *i));
+}
+
+t_cmd *ft_start_parse(char *cmd_line, char **evnp)
+{
+    t_cmd	*cmd_list;
+	t_cmd	*new_cmd;
+	char	*cmd;
+    int		i;
+	
 	i = 0;
 	cmd_list = NULL;
-	tmp = NULL;
-	while (i <= (int)ft_strlen(cmd_line) && cmd_line[i])
-	{
-		j = i;
-		i = ft_find_end(cmd_line, i);
-		cmd = malloc(sizeof(char) * (i - j + 1));
-		if (!cmd)
-			return (NULL);
-		ft_strlcpy(cmd, &cmd_line[j], i - j + 1);
-		new_cmd = init_cmd(evnp);
-		if (!new_cmd)
-		{
-			free(cmd);
-			return (NULL);
-		}
-		free(new_cmd->args);
-		new_cmd->args = ft_deal_cmd(cmd);
-		free(cmd);
-		if (!cmd_list)
-			cmd_list = new_cmd;
-		else
-			tmp->next = new_cmd;
-		tmp = new_cmd;
-	}
-	free(cmd_line);
-	return (cmd_list);
+    while (i <= (int)ft_strlen(cmd_line) && cmd_line[i])
+    {
+        cmd = extract_command(cmd_line, &i);
+        if (!cmd)
+            return (NULL);
+        new_cmd = create_and_fill_cmd(cmd, evnp);
+        free(cmd);
+        if (!new_cmd)
+            return (NULL);
+        add_command_to_list(&cmd_list, new_cmd);
+    }
+    free(cmd_line);
+    return (cmd_list);
 }
+
+
+
+// t_cmd	*ft_start_parse(char *cmd_line, char **evnp)
+// {
+// 	int		i;
+// 	int		j;
+// 	char 	*cmd;
+// 	t_cmd	*cmd_list;
+// 	t_cmd	*new_cmd;
+// 	t_cmd	*tmp;
+
+// 	i = 0;
+// 	cmd_list = NULL;
+// 	tmp = NULL;
+// 	while (i <= (int)ft_strlen(cmd_line) && cmd_line[i])
+// 	{
+// 		j = i;
+// 		i = ft_find_end(cmd_line, i);
+// 		cmd = malloc(sizeof(char) * (i - j + 1));
+// 		if (!cmd)
+// 			return (NULL);
+// 		ft_strlcpy(cmd, &cmd_line[j], i - j + 1);
+// 		new_cmd = init_cmd(evnp);
+// 		if (!new_cmd)
+// 		{
+// 			free(cmd);
+// 			return (NULL);
+// 		}
+// 		free(new_cmd->args);
+// 		new_cmd->args = ft_deal_cmd(cmd);
+// 		free(cmd);
+// 		if (!cmd_list)
+// 			cmd_list = new_cmd;
+// 		else
+// 			tmp->next = new_cmd;
+// 		tmp = new_cmd;
+// 	}
+// 	free(cmd_line);
+// 	return (cmd_list);
+// }
 
 void	ft_give_marks(t_cmd *cmd)
 {
@@ -305,6 +524,25 @@ int	ft_check_pipe(t_cmd *cmd)
 	return (0);
 }
 
+void	mark_redirects(t_cmd *start, t_cmd *end, t_cmd *target)
+{
+	t_cmd	*tmp;
+
+	tmp = start;
+	while (tmp && tmp != end->next)
+	{
+		if (tmp->input == 1)
+			target->input = 1;
+		else if (tmp->output == 1)
+			target->output = 1;
+		else if (tmp->append == 1)
+			target->append = 1;
+		else if (tmp->heredoc == 1)
+			target->heredoc = 1;
+		tmp = tmp->next;
+	}
+}
+
 t_cmd	*ft_handle_cmd_mark_v2(t_cmd *start, t_cmd *target)
 {
 	t_cmd	*end;
@@ -322,20 +560,42 @@ t_cmd	*ft_handle_cmd_mark_v2(t_cmd *start, t_cmd *target)
 		target->before_pipe = 1;
 	if (start->pipe_nbr)
 		target->after_pipe = 1;
-	while (tmp && tmp != end->next)
-	{
-		if (tmp->input == 1)
-			target->input = 1;
-		else if (tmp->output == 1)
-			target->output = 1;
-		else if (tmp->append == 1)
-			target->append = 1;
-		else if (tmp->heredoc == 1)
-			target->heredoc = 1;
-		tmp = tmp->next;
-	}
+	mark_redirects(start, end, target);
 	return (end);
 }
+
+
+// t_cmd	*ft_handle_cmd_mark_v2(t_cmd *start, t_cmd *target)
+// {
+// 	t_cmd	*end;
+// 	t_cmd	*tmp;
+
+// 	if (start == target)
+// 		tmp = start;
+// 	else
+// 		tmp = start->next;
+// 	while (tmp->next && !tmp->pipe_nbr)
+// 		tmp = tmp->next;
+// 	end = tmp;
+// 	tmp = start;
+// 	if (end->pipe_nbr)
+// 		target->before_pipe = 1;
+// 	if (start->pipe_nbr)
+// 		target->after_pipe = 1;
+// 	while (tmp && tmp != end->next)
+// 	{
+// 		if (tmp->input == 1)
+// 			target->input = 1;
+// 		else if (tmp->output == 1)
+// 			target->output = 1;
+// 		else if (tmp->append == 1)
+// 			target->append = 1;
+// 		else if (tmp->heredoc == 1)
+// 			target->heredoc = 1;
+// 		tmp = tmp->next;
+// 	}
+// 	return (end);
+// }
 
 void	ft_handle_cmd_mark(t_cmd *cmd)
 {
