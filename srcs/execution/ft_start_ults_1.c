7907a3eca25e232bfc94837d3ca06130d67a19be
Diqing Yao <dyao@c2r5s5.42wolfsburg.de>
2024-10-06 18:42:10 +0200

    make it perfect

--- srcs/execution/ft_start_ults_1.c
+++ srcs/execution/ft_start_ults_1.c
@@ -6,7 +6,7 @@
 /*   By: dyao <dyao@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 14:21:56 by dyao              #+#    #+#             */
-/*   Updated: 2024/10/04 22:24:45 by dyao             ###   ########.fr       */
+/*   Updated: 2024/10/06 18:39:53 by dyao             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,7 @@ void	ft_run_cmd(t_cmd *cmd, int **pipes, pid_t *pids)
 		pids[i_for_pid] = fork();
 		if (pids[i_for_pid] == 0)
 			ft_in_child(temp, start, pipes, &i_for_pipe);
+		sleep(1);
 		handle_parent_pipes(temp, pipes, &i_for_pipe);
 		temp = find_next_pipe(temp);
 		start = temp->next;
